e661593bebe2ff2c8087f1a7fbc6b2e82c1781e6
anedunur-amergis <anedunur@amergis.com>
2026-06-23 16:21:13 -0400

    test fix

--- src/server/__tests__/azureDevOps.test.ts
+++ src/server/__tests__/azureDevOps.test.ts
@@ -75,7 +75,7 @@ describe('AzureDevOpsService', () => {
     it('should throw error when required env variables are missing', () => {
       delete process.env.ADO_ORG;
       expect(() => new AzureDevOpsService()).toThrow(
-        'Missing required environment variables: ADO_ORG and ADO_PAT must be provided'
+        'Missing required environment variable: ADO_ORG must be provided'
       );
     });
   });
